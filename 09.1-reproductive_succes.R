b7fb7bcb983d8c2bcdca967f1d04b839480ad955
Bonnier-ju <bonnier.ju@gmail.com>
2025-06-03 17:18:01 +0200

    nb offspring by parents by clusters

--- 09.1-reproductive_succes.R
+++ 09.1-reproductive_succes.R
@@ -9,6 +9,8 @@
 library(dplyr)
 library(ggplot2)
 library(stringr)
+library(tidyr)
+
 
 plot_name <- "Nouragues"
 
@@ -51,3 +53,46 @@ clusters_plot
 # Save the plot
 output_file <- file.path(output_dir, paste0("offspring_per_cluster_", plot_name, ".png"))
 ggsave(output_file, clusters_plot, width = 8, height = 6, bg = "white" , dpi = 400)
+
+
+################## Parental success within clusters ###########################
+
+# Create ParentID–Cluster table (combine Father and Mother IDs)
+parent_contributions <- cluster_data %>%
+  select(OffspringID, ClusterIndex, FatherID, MotherID) %>%
+  pivot_longer(cols = c(FatherID, MotherID), names_to = "ParentType", values_to = "ParentID") %>%
+  filter(ParentID != "" & !is.na(ParentID)) %>%                   # remove missing
+  filter(!grepl("^#", ParentID))                                 # keep only real sampled parents
+
+# Count offspring per parent per cluster
+parent_cluster_counts <- parent_contributions %>%
+  group_by(ParentID, ClusterIndex) %>%
+  summarise(Offspring_Count = n(), .groups = "drop")
+
+# Order parents by total offspring
+parent_order <- parent_cluster_counts %>%
+  group_by(ParentID) %>%
+  summarise(Total_Offspring = sum(Offspring_Count)) %>%
+  arrange(desc(Total_Offspring)) %>%
+  pull(ParentID)
+
+# Plot
+cluster_parent_plot <- ggplot(parent_cluster_counts, aes(x = factor(ParentID, levels = parent_order),
+                                                         y = Offspring_Count,
+                                                         fill = as.factor(ClusterIndex))) +
+  geom_bar(stat = "identity") +
+  labs(title = paste0("Number of Offspring per Parent (by Cluster) – ", plot_name),
+       x = "Parent ID",
+       y = "Number of Offspring",
+       fill = "Cluster") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Display
+print(cluster_parent_plot)
+
+# Save the plot
+output_file2 <- file.path(output_dir, paste0("offspring_per_parent_by_cluster_", plot_name, ".png"))
+ggsave(output_file2, cluster_parent_plot, width = 10, height = 6, dpi = 400, bg = "white")
+
+
